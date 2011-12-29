93de0bb323bd5ee296037384bb355766d449947e
Will Brown <will.h.brown@gmail.com>
2011-12-29 15:32:54 -0600

    Twice as fast with cpython, pypy remains the same (and still much faster)

--- 43/python/euler43.py
+++ 43/python/euler43.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env pypy
+
+# Solution to Project Euler problem 43.
+# Will Brown (haldean.org)
+# Timing data:
+#   PyPy: 3.29 seconds
+#   CPython 2.7.1: 16.47 seconds
+#   CPython 3.2.2: 20.31 seconds
+
 import itertools
 
 def permutations(length):
   return itertools.permutations(str(x) for x in range(length + 1))
 
-def concat(num, d1, d2, d3):
-  return int(d(num, d1) + d(num, d2) + d(num, d3))
-
-def d(num, digit):
-  return str(num)[digit-1]
+def concat(num, d1):
+  return int(num[d1:d1+3])
 
 def match(num):
   return (
-      concat(num, 2, 3, 4) % 2 == 0 and
-      concat(num, 3, 4, 5) % 3 == 0 and
-      concat(num, 4, 5, 6) % 5 == 0 and
-      concat(num, 5, 6, 7) % 7 == 0 and
-      concat(num, 6, 7, 8) % 11 == 0 and
-      concat(num, 7, 8, 9) % 13 == 0 and
-      concat(num, 8, 9, 10) % 17 == 0)
+      concat(num, 2) % 2 == 0 and
+      concat(num, 3) % 3 == 0 and
+      concat(num, 4) % 5 == 0 and
+      concat(num, 5) % 7 == 0 and
+      concat(num, 6) % 11 == 0 and
+      concat(num, 7) % 13 == 0 and
+      concat(num, 8) % 17 == 0)
 
 def prob43():
   found = []
